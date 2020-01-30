8a9b49bab917b44ad31d3106a54d2094ce175f32
sasha walker <sashawalker31@gmail.com>
2020-01-30 03:45:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,5 @@
-def hello_t
-  i=0 
-  while i < array.length
-    yield array[i]
-    i = i+1 
-  end
-end
 
-def hello_t("Tim", "Tom", "Jim") do |name|
+def hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
